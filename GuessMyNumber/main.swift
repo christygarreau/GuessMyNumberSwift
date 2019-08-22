66d53bf9ae5fff554d148ef369bd6b8cd4789888
christygarreau <20cgarreau@castilleja.org>
2019-08-22 09:44:36 -0700

    Initial Commit

---
+++ GuessMyNumber/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GuessMyNumber
+//
+//  Created by Christy Garreau on 8/22/19.
+//  Copyright © 2019 Christy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
