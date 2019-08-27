4eede554545dea60c3ca72b26dd0fa3002bfedf9
christygarreau <20cgarreau@castilleja.org>
2019-08-27 08:29:58 -0700

    Initial Comit

--- GuessMyNumber/main.swift
+++ GuessMyNumber/main.swift
@@ -8,5 +8,73 @@
 
 import Foundation
 
-print("Hello, World!")
+class GuessMyNumberGame{
+    var guesses: Int
+    var mysteryNumber: Int
+    
+    init(){
+        self.guesses = 0
+        mysteryNumber = Int.random(in: 1...100)
+    }
+        
+    func playOneRound(){
+        print("Thinking of number between 1 and 100. Guess pls![\(mysteryNumber)]")
+        while let userInput = readLine(){
+            if let guess = Int(userInput){
+                if (guess != mysteryNumber) {
+                    if (guess < mysteryNumber){
+                        print("Too low. Try Again.")
+                    } else {
+                        print("Too high. Try again.")
+                    }
+                } else {
+                    print("You Won")
+                    return
+                }
+            } else {
+                print("Not valid. Try again")
+            }
+        }
+    }
+    
+    func play(){
+        print("Welcome")
+        var goAgain = "Y"
+        while goAgain == "Y"{
+            playOneRound()
+            mysteryNumber = Int.random(in: 1...100)
+            print("Enter Y to play agian")
+            goAgain = readLine()!
+        }
+        print("Bye!")
+    }
+}
+
+GuessMyNumberGame().play()
+
+/*
+print("Welcome to Guess My Number!")
+var goAgain = "Y"
+var mysteryNumber = Int.random(in:1...100)
+while goAgain == "Y" {
+    print("Thinking of number between 1 and 100. Guess pls! [\(mysteryNumber)]")
+    while let userInput = readLine(){ //add while unguessed or something
+        if let guess = Int(userInput){
+            
+            if (guess != mysteryNumber) {
+                if (guess < mysteryNumber){
+                    print("Too low. Try Again.")
+                } else {
+                    print("Too high. Try again.")
+                }
+            } else {
+                print("eyoo u won")
+            }
+        } else {
+            print("Not valid. Try again")
+    }
+}
+print("cya")
+}
+*/
 
